ab7b7b55c5184a1a08917fd20f3e12d19893467c
weiting <weiting_lo@tomofun.com>
2023-12-25 23:53:10 +0800

    project init

---
+++ android/app/src/main/kotlin/com/joinyou/joinyou/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.joinyou.joinyou
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app_color.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class AppColor {
+
+  static const Color transparent = Color(0x00000000);
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color grey = Color(0xFF757575);
+
+}
